dc7b3355576b7fdb25c43899fd80d6fce8b253d8
Siddharth <siddharthg@valuefy.com>
2019-07-01 07:03:39 +0530

    base changes with spotlight effect on completion

---
+++ main.js
@@ -0,0 +1,42 @@
+(function () {
+
+  const autocompleteWrapper = document.querySelector('.autocomplete');
+  const autocompleteBackground = autocompleteWrapper.querySelector('.autocomplete-background');
+  const autocompleteInput = autocompleteWrapper.querySelector('.autocomplete-input');
+
+  autocompleteInput.addEventListener('keyup', inputHandler);
+
+
+  function inputHandler(event) {
+    const inputValue = event.target.value;
+    const exisitingValues = ['google.com', "google.ocm/hello"];
+
+    const suggestions = exisitingValues.filter(item => item.toLowerCase().startsWith(inputValue.toLowerCase()));
+
+    if (suggestions.length) {
+      const casedSuggestion = suggestions[0].toLowerCase().replace(inputValue.toLowerCase(), inputValue);
+      const sliceFirst = casedSuggestion.slice(0, inputValue.length);
+      const sliceEnd = casedSuggestion.slice(inputValue.length);
+      autocompleteBackground.innerHTML = sliceFirst;
+      autocompleteBackground.setAttribute('data-autocomplete', sliceEnd);
+    } else {
+      autocompleteBackground.innerHTML = inputValue;
+      autocompleteBackground.setAttribute('data-autocomplete', '');
+    }
+
+    if (!inputValue) {
+      autocompleteBackground.innerHTML = '';
+      autocompleteBackground.setAttribute('data-autocomplete', '');
+      return;
+    }
+
+
+  }
+
+
+
+
+
+
+
+})();
